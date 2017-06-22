d29a55c768b172e17db828f56732f6934b1eb67b
afkehaya <afkehaya@gmail.com>
2017-06-22 04:47:21 -0700

    first commit

---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,6 @@
+id = 'mapbox.streets'
+
+Leaflet.tile_layer = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+# You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
+Leaflet.attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+Leaflet.max_zoom = 18
